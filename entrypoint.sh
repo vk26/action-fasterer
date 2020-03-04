73be4828f4e64beb137e728df13c3b09e7555a95
Dmitry Belyavtsev <vkosmos26@yandex.ru>
2020-03-05 00:07:20 +0800

    Set error format

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 fasterer \
-  | reviewdog -f=fasterer -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -efm="%f:%l: %m" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
