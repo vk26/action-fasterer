ed45c16b3eeb0aae5711700448aab150af5dabe5
Dmitry Belyavtsev <vkosmos26@yandex.ru>
2020-03-31 00:00:09 +0800

    Passing efm list of values

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 fasterer | sed "s/\x1b\[[0-9;]*m//g" \
-  | reviewdog -efm="%f:%l %m","%-G%.%#" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -efm="%f:%l %m" -efm="%-G%.%#" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
