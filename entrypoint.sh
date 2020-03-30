baf8cf4d3d2e8d7695009c3129ee8297862df384
Dmitry Belyavtsev <vkosmos26@yandex.ru>
2020-03-30 23:40:32 +0800

    Fix efm list

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 fasterer | sed "s/\x1b\[[0-9;]*m//g" \
-  | reviewdog -f=fasterer -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -efm="%f:%l %m","%-G%.%#" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
