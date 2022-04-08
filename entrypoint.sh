23f7136ae2af0db5d553a044d1da99a65db83c8d
carl-printreleaf <99216078+carl-printreleaf@users.noreply.github.com>
2022-04-07 20:26:16 -0600

    Handle error code for newer version of reviewdog
    This change is as per the github issue: https://github.com/reviewdog/reviewdog/issues/949

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,12 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+echo '::group:: Running fasterer with reviewdog 🐶 ...'
+
 fasterer | sed "s/\x1b\[[0-9;]*m//g" \
   | reviewdog -efm="%f:%l %m" -efm="%-G%.%#" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  
+exit_code=$?
+echo '::endgroup::'
+
+exit $exit_code
